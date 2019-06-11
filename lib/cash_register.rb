45a6ddc1e186e4f6f530db10621bcd36f489c30f
Blake Rose <bdecktor@gmail.com>
2019-06-11 01:23:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,13 +8,37 @@ class CashRegister
     @items = []
   end 
 
+  def add_item(title, price, quantity = 1)
+    if quantity > 1
+      i = 0
+      while i < quantity
+        @item << title
+        i += 1
+      end 
+    else 
+      @items << title
+    end 
+    @total += price * quantity
+       @last_transaction_amount = @total
+       @total
+     end
 
-  end
+    
+     def apply_discount()
+         if @discount > 0
+           @discount = @discount/100.to_f
+           @total = @total - (@total * (@discount))
+           "After the discount, the total comes to $#{@total.to_i}."
+         else
+           "There is no discount to apply."
+         end
+       end
 
-  def discount(percent_off)
-  end
 
-  def total(new_total_price)
-  end
+       def void_last_transaction()
+         @total -= @last_transaction_amount
+       end
+
+     end
 
 end
