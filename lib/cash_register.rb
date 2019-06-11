c2636297cd00cbab5510e5003429f63c6edb687f
Blake Rose <bdecktor@gmail.com>
2019-06-11 01:17:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
   attr_accessor :total, :discount, :last_transaction_amount, :items
 
   def initialize(cash_register_with_discount=0)
-    
+
 
   end
 
@@ -11,6 +11,6 @@ class CashRegister
   end
 
   def total(new_total_price)
-  end 
+  end
 
 end
